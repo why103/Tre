1bd719d023ba8b9c6e8b7119149e6acda2dd19b3
why <1035745999@qq.com>
2018-12-22 21:31:17 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1602/67/67feeab24cfc82bb90.water.jpg_600x330_5abf0855.jpg" />
+      <div class="banner-info">
+        <div class="banner-tittle">太平洋海底世界(AAA景区)
+        </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe663;
+          </span>39</div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/201403/07/b6ad4a8652bbade0be5885fd37b73d97.jpg_r_800x800_c83d747a.jpg', 'http://img1.qunarzz.com/sight/p0/201403/07/2800cf465b3de1667d7161ac07fa7ab5.jpg_r_800x800_b6f3b611.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+    .banner-tittle
+      flex: 1
+      font-size: .32rem
+      padding: 0 .2rem
+    .banner-number
+      height: .32rem
+      line-height: .4rem
+      margin-top: .14rem
+      padding: 0 .4rem
+      border-radius: .2rem
+      background: rgba(0, 0, 0, .8)
+      font-size: .24rem
+      .banner-icon
+        font-size: .24rem
+</style>
